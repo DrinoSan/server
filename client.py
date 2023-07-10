33dfebbb67ad8201a5acf62d09f98fe05d4e4eb7
DrinoSan <s.becirbegovic@gmail.com>
2023-07-10 21:30:03 +0200

    test client adjustmemt

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import socket               # Import socket module
-
+from time import sleep
 s = socket.socket()         # Create a socket object
 s.connect(('localhost', 8000))
-s.sendall("Hello from client\r\n\r\n".encode())
 
-msg = s.recv(19)
+s.sendall("Hello from client\r\n\r\n".encode())
+msg = s.recv(21)
 print(msg.decode())
-s.sendall("Ah Hi server".encode())
+print("-"* 40)
+
+s.sendall("Ah Hi server\r\n\r\n".encode())
+msg = s.recv(16)
+print(msg.decode())
+
+
 
 s.close()                     # Close the socket when done
