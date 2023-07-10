bd6fb7e6cc8e0ca3c56edfb225574263d40e94c0
DrinoSan <s.becirbegovic@gmail.com>
2023-07-10 19:55:33 +0200

    Adding correct http message end

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 s.connect(('localhost', 8000))
-s.sendall("Hello from client".encode())
+s.sendall("Hello from client\r\n\r\n".encode())
 
 msg = s.recv(19)
 print(msg.decode())
