1a9ef86d84603aafcf32a95c67f09c65f8d021d6
DrinoSan <s.becirbegovic@gmail.com>
2023-07-17 21:16:18 +0200

    Black formatting

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
-import socket               # Import socket module
+import socket  # Import socket module
 from time import sleep
-s = socket.socket()         # Create a socket object
-s.connect(('localhost', 8000))
+
+s = socket.socket()  # Create a socket object
+s.connect(("localhost", 8000))
 
 sendText = "GET / HTTP/1.1\r\nHost: DRINOOOOSAN\r\nHello from client\r\n\r\n"
 s.sendall(sendText.encode())
 msg = s.recv(len(sendText))
 print(msg.decode())
-print("-"* 40)
+print("-" * 40)
 
 secondSentText = "Ah Hi server\r\n\r\n"
 s.sendall(secondSentText.encode())
@@ -15,5 +16,4 @@ msg = s.recv(len(secondSentText))
 print(msg.decode())
 
 
-
-s.close()                     # Close the socket when done
+s.close()  # Close the socket when done
