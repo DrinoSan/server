3d7dcc6547e9827c174e4d57c33d0b4a543340d1
DrinoSan <s.becirbegovic@gmail.com>
2023-08-20 12:07:30 +0200

    Adding color to logging

--- src/utils/logging.cpp
+++ src/utils/logging.cpp
@@ -2,30 +2,38 @@
 #include <iostream>
 #include <string>
 
+// ANSI escape codes for text colors
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+
 //-----------------------------------------------------------------------------
-void LogInfo( int line, const char* file, const std::string format, ... )
+void LogInfo(int line, const char* file, const std::string format, ...)
 {
     const std::string formattedString =
-        static_cast<std::string>( InfoStr ) + format + "\t\t[ " +
-        std::to_string( line ) + " ]" + "\t\t[" + std::string( file ) + "]";
+        std::string(ANSI_COLOR_GREEN) + "[INFO] " + format + "\t\t[ " +
+        std::to_string(line) + " ]" + "\t\t[" + std::string(file) + "]" +
+        ANSI_COLOR_RESET;
 
     va_list args;
-    va_start( args, format );
-    vprintf( formattedString.c_str(), args );
-    va_end( args );
-    printf( "\n" );
+    va_start(args, format);
+    vprintf(formattedString.c_str(), args);
+    va_end(args);
+    printf("\n");
 }
 
 //-----------------------------------------------------------------------------
-void LogError( int line, const char* file, const std::string format, ... )
+void LogError(int line, const char* file, const std::string format, ...)
 {
     const std::string formattedString =
-        static_cast<std::string>( ErrorStr ) + format + "\t\t[ " +
-        std::to_string( line ) + " ]" + "\t\t[" + std::string( file ) + "]";
+        std::string(ANSI_COLOR_RED) + "[ERROR] " + format + "\t\t[ " +
+        std::to_string(line) + " ]" + "\t\t[" + std::string(file) + "]" +
+        ANSI_COLOR_RESET;
 
     va_list args;
-    va_start( args, format );
-    vprintf( formattedString.c_str(), args );
-    va_end( args );
-    printf( "\n" );
+    va_start(args, format);
+    vprintf(formattedString.c_str(), args);
+    va_end(args);
+    printf("\n");
 }
